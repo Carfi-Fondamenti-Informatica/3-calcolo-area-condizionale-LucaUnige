0c6427a9ad0c815aa5a9e24a2aaa0f5c69a9ca1f
Luca Sangiovanni <118013855+LucaUnige@users.noreply.github.com>
2022-11-20 15:53:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 int main() {
    float a,b;
-   int i;
+   int i=0;
    cin >>a>>b;
    cin >> i;
    switch(i){
       case 1:
          i=0;
-         cout << (a * b)/2 << endl;
+         cout << (a*b)/2 << endl;
          break;
       case 2:
          i=1;
-         cout << a * a << endl;
+         cout << a*a << endl;
          break;
       case 3:
          i=2;
-         cout << a * b << endl;
+         cout << a*b << endl;
          break;
       default:
          cout << "opzione non valida" << endl;
